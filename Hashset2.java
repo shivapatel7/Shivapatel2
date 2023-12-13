baaec14e18a1affd1e61dd62d1c7a1fdedbea8ca
shivapatel7 <121741590+shivapatel7@users.noreply.github.com>
2023-12-13 07:52:20 -0800

    Add files via upload

---
+++ Hashset2.java
@@ -0,0 +1,34 @@
+package CollectionsJava;
+
+import java.util.HashSet;
+
+public class Hashset2 {
+    public static void main(String[] args) {
+        HashSet <Integer> set1= new HashSet<Integer>();
+        set1.add(2);
+        set1.add(4);
+        set1.add(6);
+        set1.add(8);
+        set1.add(10);
+        System.out.println(set1);
+        HashSet <Integer> set2 = new HashSet<Integer>();
+        set2.add(1);
+        set2.add(2);
+        set2.add(4);
+        System.out.println(set2);
+        //union
+        //set1.addAll(set2);
+        //System.out.println(" union "+set1);
+        //Intersection
+        //set1.retainAll(set2);
+        //System.out.println("intersection "+set1);
+        //differance
+        set1.retainAll(set2);
+        System.out.println(" differance "+set1);
+        }
+
+
+
+
+    }
+
